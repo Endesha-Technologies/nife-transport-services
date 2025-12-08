6540e50d2d8aa3ba019bbe35428e118630ec745c
Aigo Faith <aigofaith@gmail.com>
2025-12-08 10:47:55 +0300

    Setup project structure

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <div className="text-center">
+        <h1 className="mb-4 text-4xl font-bold text-zinc-900 ">
+          Welcome to Nife Transport Services
+        </h1>
+        <p className="text-zinc-700 ">
+          Your reliable partner for all your transportation needs.
+        </p>
+      </div>
+    </div>
+  );
+}
